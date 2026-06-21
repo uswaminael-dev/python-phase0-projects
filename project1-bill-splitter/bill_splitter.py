70994648dd31a28a8f68853df39814eec09dbeb3
uswaminael-dev <uswaminahil.dev@gmail.com>
2026-06-21 23:45:42 +0500

    Add professional output formatting

--- project1-bill-splitter/bill_splitter.py
+++ project1-bill-splitter/bill_splitter.py
@@ -1,32 +1,31 @@
-print("========================================")
-print("        BILL SPLIT CALCULATOR        ")
-print("========================================")
+print("=" * 40)
+print("BILL SPLIT CALCULATOR".center(40))
+print("=" * 40)
 
-print("Welcome to Bill Splitter!")
+print("Welcome to Bill Splitter!")  #GREETINGS TO THE USER
 
 total_bill = float(input("Enter total bill amount: $"))
 tax_percentage = float(input("Enter tax percentage: "))
 tip_percentage = float(input("ERnter tip percentage: "))
 num_of_people = int(input("How many people: "))
 
-tax = total_bill * (tax_percentage / 100)
+tax = total_bill * (tax_percentage / 100)   #TAX CALCULATION
 
-tip = total_bill * (tip_percentage / 100)
+tip = total_bill * (tip_percentage / 100)   #TIP CALCULATION
 
-
-total_amount = total_bill + tax + tip
+total_amount = total_bill + tax + tip   #TOTAL AMOUNT CALCULATION
 
 amount_per_person = total_amount / num_of_people
 
-print("\n========================================")
-print("         CALCULATION BREAKDOWN        ")
-print("========================================")
-print("Orignal Bill: $", total_bill)
-print("Tax (", int(tax_percentage), "%): $" , tax_percentage)
-print("Tip (", int(tip_percentage), "%): $" , tip_percentage)
-print("----------------------------------------")
-print("Total Amount: $", total_amount)
-print("Number of People: " , num_of_people)
-print("----------------------------------------")
-print(f"Each person pays: ${amount_per_person:.2f}")
-print("========================================")
+print("\n=" * 40)
+print("CALCULATION BREAKDOWN.centre(40)")
+print("=" * 40)
+print(f"{'Original Bill:':<20} ${total_bill:>8.2f}")
+print(f"Tax (", int(tax_percentage), "%): ${tax:>8.2f}")
+print(f"Tip (", int(tip_percentage), "%): ${tip:>8.2f}")
+print("-" * 40)
+print(f"Total Amount: ${total_amount:>8.2f}")
+print(f"Number of People: {num_of_people:>8f}")
+print("-" * 40)
+print(f"Each person pays: ${amount_per_person:>8.2f} /person")
+print("=" * 40)
