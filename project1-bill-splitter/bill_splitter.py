0a5c9b6c92ea358c37eeb097c784e2c62aeaf20d
uswaminael-dev <uswaminahil.dev@gmail.com>
2026-06-22 13:55:46 +0500

    Add input validation with error handling

--- project1-bill-splitter/bill_splitter.py
+++ project1-bill-splitter/bill_splitter.py
@@ -4,10 +4,34 @@ print("=" * 40)
 
 print("Welcome to Bill Splitter!")  #GREETINGS TO THE USER
 
-total_bill = float(input("Enter total bill amount: $"))
-tax_percentage = float(input("Enter tax percentage: "))
-tip_percentage = float(input("ERnter tip percentage: "))
-num_of_people = int(input("How many people: "))
+def get_percentage(prompt):
+    while True:
+        try:
+            value = float(input(prompt))
+            if 0 <= value <= 100:
+                return value
+            else:
+                print("Please enter a percentage between 0 and 100.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+        
+            
+def get_positive_int(prompt):
+    while True:
+        try:
+            value = float(input(prompt))
+            if value > 0:
+                return value
+            else:
+                print("Please enter a positive number.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+    
+
+total_bill = get_positive_int("Enter the total bill amount: $")  
+tax_percentage = get_percentage("Enter tax percentage: ")
+tip_percentage = get_percentage("Enter tip percentage: ")
+num_of_people = int(get_positive_int("How many people? "))
 
 tax = total_bill * (tax_percentage / 100)   #TAX CALCULATION
 
@@ -17,15 +41,16 @@ total_amount = total_bill + tax + tip   #TOTAL AMOUNT CALCULATION
 
 amount_per_person = total_amount / num_of_people
 
-print("\n=" * 40)
-print("CALCULATION BREAKDOWN.centre(40)")
 print("=" * 40)
+print("CALCULATION BREAKDOWN".center(40))
+print("=" * 40)
+
 print(f"{'Original Bill:':<20} ${total_bill:>8.2f}")
-print(f"Tax (", int(tax_percentage), "%): ${tax:>8.2f}")
-print(f"Tip (", int(tip_percentage), "%): ${tip:>8.2f}")
+print(f"Tax {int(tax_percentage)}%: ${tax:>8.2f}")
+print(f"Tip {int(tip_percentage)}%: ${tip:>8.2f}")
 print("-" * 40)
-print(f"Total Amount: ${total_amount:>8.2f}")
-print(f"Number of People: {num_of_people:>8f}")
+print(f"{'Total Amount:':<20} ${total_amount:>8.2f}")
+print(f"{'Number of People:':<20} {num_of_people:>8}")
 print("-" * 40)
-print(f"Each person pays: ${amount_per_person:>8.2f} /person")
+print(f"{'Each person pays:':<20} ${amount_per_person:>8.2f} /person")
 print("=" * 40)
